ea6c15d506da2df886bb8a3454d69e80af7b9ef7
Tayler Geiger <taylergeiger@gmail.com>
2016-09-06 14:17:12 -0500

    can delete logs, messed w/ placeholder txt in log form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import StringField, TextAreaField, SubmitField, PasswordField, IntegerField, SelectField, BooleanField
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField, IntegerField, SelectField, BooleanField, DecimalField
 from wtforms.validators import Required, DataRequired, Email, Optional
 
 # Forms
@@ -14,9 +14,9 @@ class BrewLogForm(Form):
     origin = StringField("Coffee Origin", render_kw={"placeholder": "e.g. Kenya"})
     method = SelectField("Brew Method", choices=[('Drip', 'Drip'), ('Chemex', 'Chemex'), ('V60', 'V60'), ('Aeropress', 'Aeropress'), ('French Press', 'French Press'), ('Moka Pot', 'Moka Pot'), ('Vacuum Pot', 'Vacuum Pot'), ('Cold Brew', 'Cold Brew')])
     grind = StringField("Grind", render_kw={"placeholder": "e.g. 17 on home grinder"})
-    water = IntegerField("Water in grams")
-    coffee = IntegerField("Coffee in grams")
-    temp = IntegerField("Water Temperature")
+    water = IntegerField("Water in grams", render_kw={"placeholder": "e.g. 500"})
+    coffee = IntegerField("Coffee in grams", render_kw={"placeholder": "e.g. 17"})
+    temp = IntegerField("Water Temperature", render_kw={"placeholder": "e.g. 190"})
     flavor = TextAreaField("Tasting Notes", render_kw={"placeholder": "e.g. Fruity, acidic, floral ending notes"})
     notes = TextAreaField("Other Notes", render_kw={"placeholder": "e.g. Try lower temperature next brew, longer bloom"})
     rating = SelectField("Rating", choices=[('None', 'None'), ('1', '1: gross'), ('2', '2: bad'), ('3', '3: fine'), ('4', '4: good'), ('5', '5: great')])
